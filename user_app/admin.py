3d34ea8646f80bb3d47a655e7642f7c99a18ab79
shahriar350 <shahriar350@gmail.com>
2022-07-14 01:03:05 +0600

    cart checkout added to admin

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from user_app.models import Cart, CartProduct, CheckoutProduct, Checkout
 
 
+class CartProductAdmin(admin.TabularInline):
+    model = CartProduct
 
+
+class CheckoutProductAdmin(admin.TabularInline):
+    model = CheckoutProduct
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartProductAdmin]
+    model = Cart
+
+
+@admin.register(Checkout)
+class CheckoutAdmin(admin.ModelAdmin):
+    inlines = [CheckoutProductAdmin]
+    model = Checkout
+    list_display = ['id']
